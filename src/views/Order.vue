a6080c5ab8a09f474e564fd593d9e51186e9a084
Saeed Bagheri <Saeed.Bagheri903@gmail.com>
2018-11-01 11:28:33 +0330

    rename Transaction to Advertise

--- src/views/Order.vue
+++ src/views/Order.vue
@@ -100,11 +100,16 @@
     </div>
 </template>
 <script>
+import { mapActions } from 'vuex';
+ 
 export default {
     name: 'Order',
     data: ()=>({
 
     }),
+    mounted() {
+
+    }
 }
 </script>
 <style scoped>
