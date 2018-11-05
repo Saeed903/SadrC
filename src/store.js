80cd1189a33b063233476670e5d8d0a9c92c9773
heydar69 <heydar.bagheri69@gmail.com>
2018-11-05 17:42:56 +0330

    upgrade in vuex system in bitcoin.vue and formSell , App.vue

--- src/store.js
+++ src/store.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import feathersVuex from 'feathers-vuex';
-import feathersClient from './feathers-client'
+import feathersClient from './feathers-client';
+import router from './router';
 
 // const { 
 //   service, 
@@ -18,6 +19,36 @@ Vue.use(Vuex);
 Vue.use(FeathersVuex);
 
 export default new Vuex.Store({
+
+  state:{
+    currency:'',
+    currencyMenu: [
+      { icon: 'lightbulb_outline', text:'بیت کوین', path:'/Bitcoin'},
+      { icon: 'touch_app', text:'اتریوم', path:'/Bitcoin'},
+      { icon: 'archive', text:'ریپل', path:'/Bitcoin'},
+      { icon: 'delete', text:'زد کش', path:'/Bitcoin'},
+      { icon: 'delete', text:'زد کش1', path:'/Bitcoin'},
+      { icon: 'touch_app', text:'یک سوال دارید؟', path:'/Bitcoin'},
+    ],
+  },
+  mutations:{
+    setCurrency(state,index){
+      const selectedCurrency = state.currencyMenu.filter((item)=>item.text === index);
+      return state.currency = selectedCurrency[0].text;
+    }
+  },
+  actions:{
+    routingMenu(context,index){
+
+      const routmenu = context.state.currencyMenu.filter((i)=>i.text === index);
+
+      router.push(routmenu[0].path);
+      
+      context.commit('setCurrency', index);
+
+    }
+  },
+
   plugins:[
     service('users',{
       instanceDefaults:{
