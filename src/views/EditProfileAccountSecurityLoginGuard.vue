90d85e811e4291e0e217e5938ec670fc8249ddaa
heydar69 <heydar.bagheri69@gmail.com>
2018-11-07 17:06:40 +0330

    add vuetify grid system

--- src/views/EditProfileAccountSecurityLoginGuard.vue
+++ src/views/EditProfileAccountSecurityLoginGuard.vue
@@ -1,11 +1,19 @@
 <template>
-    <div>
-        <p>گارد ورودی</p>
-        <router-link to="">بازگشت به امنیت حساب</router-link>
-        <p>.گزینه های امنیتی برای جلوگیری از ورود سیستم های غیر مجازاست</p>
-        <v-divider></v-divider>
-        <v-checkbox>تایید ورود به سیستم از مرورگرهای ناشناخته وب</v-checkbox>
-        <v-btn @click="submit" class="primary">تغییر تنظیمات</v-btn>
-        <p>.هر گونه تغییر به این تنظیمات نیاز به تایید ایمیل دارد</p>
-    </div>
+<v-container fluid grid-list-md>
+    <v-layout column>
+        <v-flex d-flex>
+            <v-card color="indigo" dark>
+                <v-card-text>
+                    <p>گارد ورودی</p>
+                    <router-link to="/EditProfileIdentifyVerify">بازگشت به امنیت حساب</router-link>
+                    <p>.گزینه های امنیتی برای جلوگیری از ورود سیستم های غیر مجازاست</p>
+                    <v-divider></v-divider>
+                    <v-checkbox>تایید ورود به سیستم از مرورگرهای ناشناخته وب</v-checkbox>
+                    <v-btn @click="submit" class="primary" to="/EditProfileChangeEmail">تغییر تنظیمات</v-btn>
+                    <p>.هر گونه تغییر به این تنظیمات نیاز به تایید ایمیل دارد</p>
+                </v-card-text>
+            </v-card>
+        </v-flex>
+    </v-layout>
+</v-container>
 </template>
