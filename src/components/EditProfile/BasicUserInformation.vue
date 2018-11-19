5265bd3c80a742e2ddeb512129f8e74e5427e7a2
farzad8326 <farzad6238afsari>
2018-11-19 15:28:26 +0330

    create a second part of the edit profile

--- src/components/EditProfile/BasicUserInformation.vue
+++ src/components/EditProfile/BasicUserInformation.vue
@@ -12,103 +12,82 @@
             label="من را بعنوان تایید کننده نام واقعی به دیگران نشان بده"
             >
             </v-switch>
-            <span>نام کاربری شما زمانی نمایش داده می شوند که افراد با شرکای تجاری خود معاملات جدیدی را انجام میدهند</span>
+            <span class="fontIran subheading">نام کاربری شما زمانی نمایش داده می شوند که افراد با شرکای تجاری خود معاملات جدیدی را انجام میدهند</span>
         </v-card>
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        <div>
-
+        <v-card>
             <v-switch
-            color="aqua"
-            label="اطلاعات حساس را از اطلاعیه های ایمیل غیر فعال کنید."
+            color="cyan accent-2"
+            class="fontIran body-1"
+            label="اطلاعات حساس را از اطلاعیه های ایمیل غیر فعال کنید"
             >
             </v-switch>
-            <span class="userNameShown">ایمیل فقظ از شما می خواهد که به سایت وارد شوید.جایی که اطلاعیه های واقعی باشد.</span>
-        </div>
-
-        <div>
+            <span class="fontIran subheading">ایمیل فقظ از شما می خواهد که به سایت وارد شوید.جایی که اطلاعیه های واقعی باشد.</span>
+        </v-card>
+        <v-card>
             <v-switch
-            color="aqua"
+            color="cyan accent-2"
+            class="fontIran body-1"
             label="اعلان های وب را فعال کنید."
             >
             </v-switch>
-            <span>شما اطلاعیه های خارجی برای فروش را بطور  موقت دریافت خواهید کرد.</span>
-        </div>
-
-        <div>
+            <span class="fontIran subheading">شما اطلاعیه های خارجی برای فروش را بطور  موقت دریافت خواهید کرد.</span>
+        </v-card>
+        <v-card>
             <v-switch
-            color="aqua"
+            color="cyan accent-2"
+            class="fontIran body-1"
             label="فروش در تعطبلات"
             >
             </v-switch>
-            <span>همه تبلیغات خود را برای خرید به طور موقت غیر فغال کنید.</span>
-        </div>
-
-        <div>
+            <span class="fontIran subheading">همه تبلیغات خود را برای خرید به طور موقت غیر فغال کنید.</span>
+        </v-card>
+        <v-card>
             <v-switch
-            color="aqua"
+            color="cyan accent-2"
+            class="fontIran body-1"
             label="خرید در تعطیلات"
             >
             </v-switch>
-            <span class="userNameShown">همه تبلیغات خود را برای خرید به طور موقت غیر فغال کنید.</span>
-        </div>
-
-        <div>
+            <span class="fontIran subheading">همه تبلیغات خود را برای خرید به طور موقت غیر فغال کنید.</span>
+        </v-card>
+        <v-card>
             <v-switch
-            color="aqua"
-            label=" ارسال پیامک برای مخاطبین جدید تجارت"
+            color="cyan accent-2"
+            class="fontIran body-1"
+            label="ارسال پیامک برای مخاطبین جدید تجارت"
             >
             </v-switch>
-            <span>ارسال پیامک از درخواست های تماس جدید</span>
-        </div>
-
-        <div>
+            <span class="fontIran subheading">ارسال پیامک از درخواست های تماس جدید</span>
+        </v-card>
+        <v-card>
             <v-switch
-            color="aqua"
+            color="cyan accent-2"
+            class="fontIran body-1"
             label="ارسال پیامک برای پرداخت های جدید آنلاین"
             >
             </v-switch>
-            <span>ارسال پیامک برای پرداخت های آنلاین در تبلیغات شما</span>
-        </div>
-
-        <div>
+            <span class="fontIran subheading">ارسال پیامک برای پرداخت های آنلاین در تبلیغات شما</span>
+        </v-card>
+        <v-card>
             <v-switch
-            color="aqua"
+            color="cyan accent-2"
+            class="fontIran body-1"
             label=""
             >
             </v-switch>
-            <span>ارسال پیامک از نسخه های جدید آنلاین سپرو</span>
-        </div>
-
-        <div>
-            <v-flex xs6>
+            <span class="fontIran subheading">ارسال پیامک از نسخه های جدید آنلاین سپرو</span>
+        </v-card>
+        <v-flex xs6>
+            <v-card>
                 <v-textarea
                 box
                 label="معرفی"
                 hint="در پروفایل عمومی شما نشان داده می شود.تا سقف 200 کاراکتر متن نوشته شود."
                 >
                 </v-textarea>
-            </v-flex>
-            <v-btn 
-            color="blue"
-            round
-            >
-            ذخیره مشخصات
-            </v-btn>
-        </div>
+                <v-btn color="blue" round>ذخیره مشخصات</v-btn>
+            </v-card>
+        </v-flex>
     </div>
 </template>
 <script>
