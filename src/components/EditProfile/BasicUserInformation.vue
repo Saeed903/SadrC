c926829baf012986ad12865bc3313de51e54095e
farzad8326 <farzad6238afsari>
2018-11-19 10:23:33 +0330

    css

--- src/components/EditProfile/BasicUserInformation.vue
+++ src/components/EditProfile/BasicUserInformation.vue
@@ -1,83 +1,96 @@
 <template>
     <div>
+        <v-card>
+            <v-card-text class="fontIran text-xs-right subheading">اطلاعات پایه کاربر</v-card-text>
+            <span class="fontIran caption mr-3">ایمیل:farzad6238afsari@gmail.com(تایید نشده)</span>
+        </v-card>
         
-         <div class="backInfoTop">
-            <p class="basicInfo">اطلاعات پایه کاربر</p>
-            <p class="basicEmail">ایمیل:farzad6238afsari@gmail.com(تایید نشده)</p>
-        </div>
-
-       
-
-        <div class="showRealName">
+        <v-card>
             <v-switch
-            color="blue"
+            color="cyan accent-2"
+            class="fontIran body-1"
             label="من را بعنوان تایید کننده نام واقعی به دیگران نشان بده"
             >
             </v-switch>
-            <span class="userNameShown">نام کاربری شما زمانی نمایش داده می شوند که افراد با شرکای تجاری خود معاملات جدیدی را انجام میدهند</span>
-        </div>
+            <span>نام کاربری شما زمانی نمایش داده می شوند که افراد با شرکای تجاری خود معاملات جدیدی را انجام میدهند</span>
+        </v-card>
+  
 
-        <div class="showRealName">
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <div>
+
             <v-switch
-            color="blue"
+            color="aqua"
             label="اطلاعات حساس را از اطلاعیه های ایمیل غیر فعال کنید."
             >
             </v-switch>
             <span class="userNameShown">ایمیل فقظ از شما می خواهد که به سایت وارد شوید.جایی که اطلاعیه های واقعی باشد.</span>
         </div>
 
-        <div class="showRealName">
+        <div>
             <v-switch
-            color="blue"
+            color="aqua"
             label="اعلان های وب را فعال کنید."
             >
             </v-switch>
-            <span class="userNameShown">شما اطلاعیه های خارجی برای فروش را بطور  موقت دریافت خواهید کرد.</span>
+            <span>شما اطلاعیه های خارجی برای فروش را بطور  موقت دریافت خواهید کرد.</span>
         </div>
 
-        <div class="showRealName">
+        <div>
             <v-switch
-            color="blue"
+            color="aqua"
             label="فروش در تعطبلات"
             >
             </v-switch>
-            <span class="userNameShown">همه تبلیغات خود را برای خرید به طور موقت غیر فغال کنید.</span>
+            <span>همه تبلیغات خود را برای خرید به طور موقت غیر فغال کنید.</span>
         </div>
 
-        <div class="showRealName">
+        <div>
             <v-switch
-            color="blue"
+            color="aqua"
             label="خرید در تعطیلات"
             >
             </v-switch>
             <span class="userNameShown">همه تبلیغات خود را برای خرید به طور موقت غیر فغال کنید.</span>
         </div>
 
-        <div class="showRealName">
+        <div>
             <v-switch
-            color="blue"
+            color="aqua"
             label=" ارسال پیامک برای مخاطبین جدید تجارت"
             >
             </v-switch>
-            <span class="userNameShown">ارسال پیامک از درخواست های تماس جدید</span>
+            <span>ارسال پیامک از درخواست های تماس جدید</span>
         </div>
 
-        <div class="showRealName">
+        <div>
             <v-switch
-            color="blue"
+            color="aqua"
             label="ارسال پیامک برای پرداخت های جدید آنلاین"
             >
             </v-switch>
-            <span class="userNameShown">ارسال پیامک برای پرداخت های آنلاین در تبلیغات شما</span>
+            <span>ارسال پیامک برای پرداخت های آنلاین در تبلیغات شما</span>
         </div>
 
-        <div class="showRealName">
+        <div>
             <v-switch
-            color="blue"
+            color="aqua"
             label=""
             >
             </v-switch>
-            <span class="userNameShown">ارسال پیامک از نسخه های جدید آنلاین سپرو</span>
+            <span>ارسال پیامک از نسخه های جدید آنلاین سپرو</span>
         </div>
 
         <div>
@@ -85,7 +98,6 @@
                 <v-textarea
                 box
                 label="معرفی"
-                class="textarea"
                 hint="در پروفایل عمومی شما نشان داده می شود.تا سقف 200 کاراکتر متن نوشته شود."
                 >
                 </v-textarea>
@@ -103,39 +115,7 @@
     
 </script>
 <style scoped>
-.backInfoTop{
-    margin-top:22px;
+.fontIran{
+    font-family:iranian sans;
 }
-.basicInfo{
-    font-size:25px;
-    font-family:b nazanin;
-    font-weight:bold;
-    color:rgb(78, 73, 73);
-    text-align:left;
-    text-shadow:1px 3px 3px rgb(201, 183, 183);
-}
-.basicEmail{
-    font-size:18px;
-    font-family:b nazanin;
-    color:rgb(78, 73, 73);
-    text-align:left;
-    text-shadow:1px 3px 3px rgb(201, 183, 183);
-}
-.selectPercentage{
-    width:35%;
-}
-.showRealName{
-    padding-bottom:25px;
-}
-.userNameShown{
-    font-size:18px;
-    font-family:b nazanin;
-    color:rgb(121, 113, 113);
-    padding-top:0;
-}
-.textarea{
-    padding-top:25px;
-}
-
-
 </style>
