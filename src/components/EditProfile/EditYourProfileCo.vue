527cccc96e388adb0ad2afdf311918897c27d44b
farzad8326 <farzad6238afsari>
2018-11-20 15:54:36 +0330

    create the edit your profile component and finish

--- src/components/EditProfile/EditYourProfileCo.vue
+++ src/components/EditProfile/EditYourProfileCo.vue
@@ -1,85 +1,51 @@
 <template>
     <div>
-        <div>
-            <p class="editProfileText">ویرایش پروفایل</p>
-        </div>
+        <v-layout row wrap>
+            <v-card-text class="fontIran text-xs-center">ویرایش پروفایل</v-card-text>
 
-        <div class="topInformationBox">
-            <router-link class="topBoxText" to="">اطلاعات پایه کاربر</router-link><br>
-            <router-link class="topBoxText" to="">تغییر رمز عبور</router-link><br>
-            <router-link class="topBoxText" to="">تغییر آدرس ایمیل</router-link><br>
-            <router-link class="topBoxText" to="">تایید</router-link><br>
-            <router-link class="topBoxText" to="">امنیت</router-link><br>
-            <router-link class="topBoxText" to="">اسم واقعی</router-link><br>
-        </div>
+            <v-flex xs12 sm3 md2>
+                <v-btn class="pr-3 fontIran sm4" color="info caption" round to="">اطلاعات پایه کاربر</v-btn>
+            </v-flex>
+            <v-flex xs12 sm3 md2>
+                <v-btn class="pr-3 fontIran sm4" color="info caption" round to="">تغییر رمز عبور</v-btn>
+            </v-flex>
+            <v-flex xs12 sm3 md2>
+                <v-btn class="pr-3 fontIran sm4" color="info caption" round to="">تغییر آدرس ایمیل</v-btn>
+            </v-flex>
+            <v-flex xs12 sm3 md2>
+                <v-btn class="pr-3 fontIran sm4" color="info caption" round to="">تایید</v-btn>
+            </v-flex>
+            <v-flex xs12 sm3 md2> 
+                <v-btn class="pr-3 fontIran sm4" color="info caption" round to="">امنیت</v-btn>
+            </v-flex>
+            <v-flex xs12 sm3 md2>
+                <v-btn class="pr-3 fontIran sm4" color="info caption" round to="">اسم واقعی</v-btn>
+            </v-flex>
+        </v-layout>
 
-        <div class="resendEmail">
-            <span class="resendEmailText"><span class="surprise">!</span>شما هنوز ایمیل خود را تایید نکرده اید شما حدود 2 روز و 22 ساعت تا زمانی که ایمیل شما تایید شده است.لطفا به زودی صندوق پستی را برای تاییدذ بررسی کنید</span>
-            <v-btn color="info" round to="">ارسال آدرس ایمیل تایید شده</v-btn>
-        </div>
-
-        <div class="resendEmail">
-            <span class="resendEmailText"><span class="surprise">!</span>شما شماره تلفن تایید شده را به حساب کاربری خود اضافه نکرده اید</span>
-            <v-btn color="info" round to="">تایید شماره تلفن</v-btn>
-        </div>
-
-        <div class="resendEmail">
-            <div>
-                <span class="resendEmailText"><span class="surprise">!</span>شما هنوز نام واقعی خود را به حساب کاربری خود تنظیم نکرده اید.</span>
-                <v-btn color="info" round to="">ایجاد نام واقعی</v-btn>
-            </div>
-        </div>
-
-
+        <v-card-text class="fontIran font-weight-medium caption pt-4"><v-icon color="red" size="18px">warning</v-icon>شما هنوز ایمیل خود را تایید نکرده اید شما حدود 2 روز و 22 ساعت تا زمانی که ایمیل شما تایید شده است.لطفا به زودی صندوق پستی را برای تاییدذ بررسی کنید.<router-link to="" class="fontsIran caption">ارسال آدرس ایمیل</router-link></v-card-text>
+        <v-card-text class="fontIran font-weight-medium caption pt-0"><v-icon color="red" size="18px">warning</v-icon>شما شماره تلفن تایید شده را به حساب کاربری خود اضافه نکرده اید.<router-link to="" class="fontsIran caption">تایید شماره تلفن</router-link></v-card-text>
+        <v-card-text class="fontIran font-weight-medium caption pt-0"><v-icon color="red" size="18px">warning</v-icon>شما هنوز نام واقعی خود را به حساب کاربری خود تنظیم نکرده اید.<router-link to="" class="fontsIran caption">ایجاد نام واقعی</router-link></v-card-text>
+        
     </div>
 </template>
 <script>
-    
+    export default{
+        
+    }
 </script>
 <style scoped>
-.editProfileText{
-    text-align:right;
-    font-size:25px;
-    font-family:b nazanin;
-    font-weight:bold;
-    color:rgb(78, 73, 73);
-    text-shadow:1px 3px 3px rgb(207, 194, 194);
+.fontIran{
+    font-family:'Iranian Sans';
+    padding-left:20px;
 }
-.topInformationBox{
-    border:2px solid rgb(214, 206, 206);
-    border-radius:5px;
-    background-color:rgb(243, 240, 240);
-    width:100%;
-    margin-right:auto;
-    margin-left:auto;
-}
-.topBoxText{
+.fontsIran{
+    font-family:'iranian sans';
+    color:rgb(0, 153, 255);
     text-decoration:none;
-    color:rgb(91, 31, 230);
-    font-size:16px;
-    font-family:b nazanin;
-    padding-left:10px;
 }
-.topBoxText:hover{
-    text-decoration:underline;
-    color:rgb(22, 40, 201);
-}
-.resendEmail{
-    background-color:rgb(201, 235, 243);
-    border-radius:5px;
-    border:2px solid rgb(151, 243, 243);   
-    margin-top:22px;
-}
-.resendEmailText{
-    font-size:18px;
-    font-family:b nazanin;
-    color:rgb(69, 49, 184);
-    text-shadow:1px 3px 3px rgb(206, 184, 184);
-    padding-left:5px;
-    padding-top:6px;
-}
-.surprise{
-    font-weight:bold;
+.fontsIran:hover{
+    color:rgb(0, 140, 255);
 }
 
 </style>
