9d7c449803f40f43e0975bff708a1f2519b9726e
farzad8326 <farzad6238afsari>
2018-11-20 16:44:23 +0330

    a bit change in this page

--- src/components/EditProfile/EditYourProfileCo.vue
+++ src/components/EditProfile/EditYourProfileCo.vue
@@ -1,13 +1,18 @@
 <template>
     <div>
+        <v-card>
         <v-layout row wrap>
             <v-card-text class="fontIran text-xs-center">ویرایش پروفایل</v-card-text>
 
             <v-flex xs12 sm3 md2>
-                <v-btn class="pr-3 fontIran sm4" color="info caption" round to="">اطلاعات پایه کاربر</v-btn>
+                <v-card>
+                    <v-btn class="pr-3 fontIran sm4" color="info caption" round to="">اطلاعات پایه کاربر</v-btn>
+                </v-card>
             </v-flex>
             <v-flex xs12 sm3 md2>
-                <v-btn class="pr-3 fontIran sm4" color="info caption" round to="">تغییر رمز عبور</v-btn>
+                <v-card>
+                    <v-btn class="pr-3 fontIran sm4" color="info caption" round to="">تغییر رمز عبور</v-btn>
+                </v-card>
             </v-flex>
             <v-flex xs12 sm3 md2>
                 <v-btn class="pr-3 fontIran sm4" color="info caption" round to="">تغییر آدرس ایمیل</v-btn>
@@ -26,12 +31,12 @@
         <v-card-text class="fontIran font-weight-medium caption pt-4"><v-icon color="red" size="18px">warning</v-icon>شما هنوز ایمیل خود را تایید نکرده اید شما حدود 2 روز و 22 ساعت تا زمانی که ایمیل شما تایید شده است.لطفا به زودی صندوق پستی را برای تاییدذ بررسی کنید.<router-link to="" class="fontsIran caption">ارسال آدرس ایمیل</router-link></v-card-text>
         <v-card-text class="fontIran font-weight-medium caption pt-0"><v-icon color="red" size="18px">warning</v-icon>شما شماره تلفن تایید شده را به حساب کاربری خود اضافه نکرده اید.<router-link to="" class="fontsIran caption">تایید شماره تلفن</router-link></v-card-text>
         <v-card-text class="fontIran font-weight-medium caption pt-0"><v-icon color="red" size="18px">warning</v-icon>شما هنوز نام واقعی خود را به حساب کاربری خود تنظیم نکرده اید.<router-link to="" class="fontsIran caption">ایجاد نام واقعی</router-link></v-card-text>
-        
+        </v-card>
     </div>
 </template>
 <script>
     export default{
-        
+
     }
 </script>
 <style scoped>
