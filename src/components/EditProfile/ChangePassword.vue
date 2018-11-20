35cc37630b186d389c28ad72613c6dfb1a5d5d7e
farzad8326 <farzad6238afsari>
2018-11-20 16:54:15 +0330

    css

--- src/components/EditProfile/ChangePassword.vue
+++ src/components/EditProfile/ChangePassword.vue
@@ -1,59 +1,55 @@
 <template>
     <div>
-          <div >
-            <span class="twoCharacters">تغییر رمز عبور</span><br>
-            <router-link class="routerCharacters" to="">تغییر رمز عبور</router-link>
+            <v-card-text class="fontIran">تغییر رمز عبور</v-card-text>
+            <router-link class="fontsIran" to="">تغییر رمز عبور</router-link>
+
+            <v-card-text class="fontIran">تغییر آدرس ایمیل</v-card-text>
+            <router-link class="fontsIran" to="">تغییر آدرس ایمیل</router-link>
+
+        <div >
+            <v-card-text class="fontIran">ارزیابی</v-card-text>
+            <v-card-text class="fontIran">آدرس ایمیل تایید شده:نه</v-card-text>
+            <router-link class="fontsIran" to="">راه اندازی ایمیل</router-link>
+            <v-card-text class="fontIran">شماره تلفن تایید شده:نه</v-card-text>
+            <router-link class="fontsIran" to="">تایید شماره تلفن</router-link>
+            <v-card-text class="fontIran">شناسایی تایید شده:نه</v-card-text>
+            <router-link class="fontsIran" to="">هویت را تایید کنید</router-link>
         </div>
 
         <div >
-            <span class="twoCharacters">تغییر آدرس ایمیل</span><br>
-            <router-link class="routerCharacters" to="">تغییر آدرس ایمیل</router-link>
+            <v-card-text class="fontIran">امنیت</v-card-text>
+            <v-card-text class="fontIran">سطح امنیتی حساب شما ضعیف اسا</v-card-text>
+            <router-link class="fontsIran" to="">ویرایش گزینه های امنیتی</router-link> 
         </div>
 
         <div >
-            <span class="twoCharacters">ارزیابی</span><br>
-            <span class="textCharacters">آدرس ایمیل تایید شده:نه</span><br>
-            <router-link class="routerCharacters" to="">راه اندازی ایمیل</router-link><br>
-            <span class="textCharacters">شماره تلفن تایید شده:نه</span><br>
-            <router-link class="routerCharacters" to="">تایید شماره تلفن</router-link><br>
-            <span class="textCharacters">شناسایی تایید شده:نه</span><br>
-            <router-link class="routerCharacters" to="">هویت را تایید کنید</router-link>
+            <v-card-text class="fontIran">اسم واقعی</v-card-text>
+            <v-card-text class="fontIran">تایید اعتبار اسم:تایید نشده</v-card-text>
+            <router-link class="fontsIran" to="">اطلاعات نام واقعی شما</router-link>
         </div>
 
         <div >
-            <span class="twoCharacters">امنیت</span><br>
-            <span class="textCharacters">سطح امنیتی حساب شما ضعیف اسا</span><br>
-            <router-link class="routerCharacters" to="">ویرایش گزینه های امنیتی</router-link><br>
+            <v-card-text class="fontIran">برنامه ها</v-card-text>
+            <v-card-text class="fontIran">برنامه های شضص ثاثت میتوانند عملیاتی را برای شمادر صدر کریپتو انجامدهد</v-card-text>
+            <v-card-text class="fontIran">شما در حال حاضر از هیچ برنامه ای استفاده نمی کنید</v-card-text>
+            <router-link class="fontsIran" to="">تنظیم و تغییر کد پین برنامه</router-link>
+            <router-link class="fontsIran" to="">مدیریت برنامه ها</router-link>
         </div>
 
         <div >
-            <span class="twoCharacters">اسم واقعی</span><br>
-            <span class="textCharacters">تایید اعتبار اسم:تایید نشده</span><br>
-            <router-link class="routerCharacters" to="">اطلاعات نام واقعی شما</router-link><br>
+            <v-card-text class="fontIran">دریافت خدمات حق بیمه</v-card-text>
+            <router-link class="fontsIran" to="">دریافت های خود را برای خدمات صدرکریپتو مشاده کنید.</router-link><br>
+            <v-card-text class="fontIran"><strong class="fontIran">توجه:</strong>از معاملات شما دریافتی نشده است اما دریافتی از خدماتی که اط صدرکریپتو خریداری کرده اید برای اهداف مالیاتی است شما میتوانید کسب و کار خود را بذست اوردن مخاطبین گیدا کنید</v-card-text>
         </div>
 
         <div >
-            <span class="twoCharacters">برنامه ها</span><br>
-            <span class="textCharacters">برنامه های شضص ثاثت میتوانند عملیاتی را برای شمادر صدر کریپتو انجامدهد</span><br>
-            <span class="textCharacters">شما در حال حاضر از هیچ برنامه ای استفاده نمی کنید</span><br>
-            <router-link class="routerCharacters" to="">تنظیم و تغییر کد پین برنامه</router-link><br>
-            <router-link class="routerCharacters" to="">مدیریت برنامه ها</router-link>
+            <v-card-text class="fontIran">اطلاعات شخصی</v-card-text>
+            <router-link class="fontsIran" to="">نشان کردن اطلاعات شخصی</router-link>
         </div>
 
         <div >
-            <span class="twoCharacters">دریافت خدمات حق بیمه</span><br>
-            <router-link class="routerCharacters" to="">دریافت های خود را برای خدمات صدرکریپتو مشاده کنید.</router-link><br>
-            <span class="textCharacters"><strong class="textCharacters">توجه:</strong>از معاملات شما دریافتی نشده است اما دریافتی از خدماتی که اط صدرکریپتو خریداری کرده اید برای اهداف مالیاتی است شما میتوانید کسب و کار خود را بذست اوردن مخاطبین گیدا کنید</span><br>
-        </div>
-
-        <div >
-            <span class="twoCharacters">اطلاعات شخصی</span><br>
-            <router-link class="routerCharacters" to="">نشان کردن اطلاعات شخصی</router-link>
-        </div>
-
-        <div >
-            <span class="twoCharacters">اطلاعات حساب</span><br>
-            <router-link class="routerCharacters" to="">حذف حساب کاربری در صدر کریپتو</router-link>
+            <v-card-text class="fontIran">اطلاعات حساب</v-card-text>
+            <router-link class="fontsIran" to="">حذف حساب کاربری در صدر کریپتو</router-link>
         </div>
  
     </div>
@@ -63,33 +59,16 @@
 </script>
 
 <style scoped>
-
-.twoCharacters{
-    font-size:21px;
-    font-weight:bold;
-    font-family:b nazanin;
-    color:rgb(34, 31, 31);
-    text-shadow:1px 3px 3px rgb(168, 156, 156);
+.fontIran{
+    font-family:'Iranian Sans';
+    padding-left:20px;
 }
-.routerCharacters{
+.fontsIran{
+    font-family:'iranian sans';
+    color:rgb(0, 153, 255);
     text-decoration:none;
-    text-indent:55px;
-    font-size:17px;
-    font-family:b nazanin;
-    color:rgb(15, 123, 224);
 }
-.routerCharacters:hover{
-    text-decoration:underline;
-    color:rgb(3, 129, 247);
-    text-shadow:1px 3px 3px rgb(179, 167, 167);
+.fontsIran:hover{
+    color:rgb(0, 140, 255);
 }
-.textCharacters{
-    font-size:17px;
-    font-family:b nazanin;
-    color:rgb(5, 5, 5);
-}
-div{
-    padding-top:30px;
-}
-
 </style>
