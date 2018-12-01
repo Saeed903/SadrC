5453b31bba9978912bca5740b10015847ccd8be6
heydar69 <heydar.bagheri69@gmail.com>
2018-12-01 11:08:19 +0330

    css and gride vuetify

--- src/views/Guides.vue
+++ src/views/Guides.vue
@@ -1,65 +1,92 @@
 <template>
-<v-container grid-list-xl>
-    <v-layout column align-justify justify-center>
-       
-            <v-card-text class="text-xs-center">
-                <v-flex xs12 sm6 md6 lg6 xl6>
-                    <v-card-text>
-                        <v-icon>description</v-icon>
-                        <p>راهنمایی درصدر کرپیتو</p>
-                        <p>خودتان را با راهنمایی های ما راحت کنید</p>
-                    </v-card-text>
-                </v-flex>
-                <v-flex xs12 sm6 md6 lg6 xl6>
-                    <v-card>
-                        <v-card-text>
-                            <p>چگونه بیت کوین بخریم؟</p>
-                            <p>خودتان را با راهنمایی های ما راحت کنید</p>
-                        </v-card-text>
-                    </v-card>
-                </v-flex>
-                <v-flex xs12 sm6 md6 lg6 xl6>
-                    <v-card>
-                        <v-card-text>
-                            <p>چگونه بیت کوین بخریم؟</p>
-                            <p>خودتان را با راهنمایی های ما راحت کنید</p>
-                        </v-card-text>
-                    </v-card>
-                </v-flex>
-                <v-flex xs12 sm6 md6 lg6 xl6>
-                    <v-card>
-                        <v-card-text>
-                            <p>چگونه بیت کوین بخریم؟</p>
-                            <p>خودتان را با راهنمایی های ما راحت کنید</p>
-                        </v-card-text>
-                    </v-card>
-                </v-flex>
-                <v-flex xs12 sm6 md6 lg6 xl6>
-                    <v-card>
-                        <v-card-text>
-                            <p>چگونه بیت کوین بخریم؟</p>
-                            <p>خودتان را با راهنمایی های ما راحت کنید</p>
-                        </v-card-text>
-                    </v-card>
-                </v-flex>
-                <v-flex xs12 sm6 md6 lg6 xl6>
-                    <v-card>
-                        <v-card-text>
-                            <p>چگونه بیت کوین بخریم؟</p>
-                            <p>خودتان را با راهنمایی های ما راحت کنید</p>
-                        </v-card-text>
-                    </v-card>
-                </v-flex>
-                <v-flex xs12 sm6 md6 lg6 xl6>
-                    <v-card>
-                        <v-card-text>
-                            <p>چگونه بیت کوین بخریم؟</p>
-                            <p>خودتان را با راهنمایی های ما راحت کنید</p>
-                        </v-card-text>
-                        </v-card>
-                </v-flex>
+    <div class="text-xs-center fontIran ">
+        <v-flex xs12 sm12 md8 lg6 xl4>
+            <v-card-text>
+                <v-icon>description</v-icon>
+                <p class="titled">راهنمایی درصدر کرپیتو</p>
+                <p>خودتان را با راهنمایی های ما راحت کنید</p>
             </v-card-text>
-        
-    </v-layout>
-</v-container>
+            
+        </v-flex>
+        <v-flex xs12 sm12 md8 lg6 xl4>
+            <v-card class="mb-3">
+                <v-card-text>
+                    <p class="headlined">چگونه بیت کوین بخریم؟</p>
+                    <p>خودتان را با راهنمایی های ما راحت کنید</p>
+                </v-card-text>
+            </v-card>
+        </v-flex>
+        <v-flex xs12 sm12 md8 lg6 xl4>
+            <v-card class="mb-3">
+                <v-card-text>
+                    <p class="headlined">چگونه بیت کوین بفروشیم؟</p>
+                    <p>چگونه بیت کوین خود را به سرعت و با خیال راحت بفروشید؟</p>
+                </v-card-text>
+            </v-card>
+        </v-flex>
+        <v-flex xs12 sm12 md8 lg6 xl4>
+            <v-card class="mb-3">
+                <v-card-text>
+                    <p class="headlined">راهنمای امنیت</p>
+                    <p>راهنمای نگه داشتن خود و پول خود را امن نگه دارید </p>
+                </v-card-text>
+            </v-card>
+        </v-flex>
+        <v-flex xs12 sm12 md8 lg6 xl4>
+            <v-card class="mb-3">
+                <v-card-text>
+                    <p class="headlined">مقدمه ای برای تجارت بیت کوین آنلاین </p>
+                    <p>مقدمه راهنمایی برای تجارت آنلاین بیت کوین در صدر کریپتو</p>
+                </v-card-text>
+            </v-card>
+        </v-flex>
+        <v-flex xs12 sm12 md8 lg6 xl4>
+            <v-card class="mb-3">
+                <v-card-text>
+                    <p class="headlined">راهنمایی در مورد چگونگی تجارت بیت کوین در محلی</p>
+                    <p>راهنمای کوتاه در مورد نحوه فروش بیت کوین به صورت محلی برای پول نقد</p>
+                </v-card-text>
+            </v-card>
+        </v-flex>
+        <v-flex xs12 sm12 md8 lg6 xl4>
+            <v-card class="mb-3">
+                <v-card-text>
+                    <p class="headlined">معادلات قیمت تبلیغات 101 </p>
+                    <p>اصول اولیه قیمت تبلیغات را یاد بگیرید</p>
+                </v-card-text>
+                </v-card>
+        </v-flex>
+        <v-flex xs12 sm12 md8 lg6 xl4>
+            <v-card class="mb-3">
+                <v-card-text>
+                    <p class="headlined">مستندات آی پی </p>
+                    <p>از آی پی استفاده کنید تا برنامه های خودتان را بسازید و به تجارت خودکار بپردازید</p>
+                </v-card-text>
+                </v-card>
+        </v-flex>
+        <v-flex xs12 sm12 md8 lg6 xl4>
+            <v-card class="mb-3">
+                <v-card-text>
+                    <p class="headlined">بیت کوین ها را در وب سایت خود بپذیرید</p>
+                    <p>نحوه استفاده از آی پی برای پذیرش بیت کوین ها در فروشگاه وب شما چیست؟</p>
+                </v-card-text>
+                </v-card>
+        </v-flex>
+    </div>
 </template>
+<style scoped>
+.titled{
+  font-family: Iranian Sans;
+  font-size: 28px; 
+}
+
+.headlined{
+  font-family: Iranian Sans;
+  font-size: 22px; 
+}
+.fontIran{
+font-family: Iranian Sans;
+font-size: 12px;
+}
+
+</style>
