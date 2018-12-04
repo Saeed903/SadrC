c17088b178da2ff47443480269cc991b6bb237ce
saeed903 <saeed.bagheri903@gmail.com>
2018-12-04 09:51:17 +0330

    change the topic of this page

--- src/views/AboutUs.vue
+++ src/views/AboutUs.vue
@@ -4,7 +4,14 @@
             <v-flex lg10 xl8>
 
                 <v-layout row wrap>
-                    <v-flex sm7 xs7 md7 xl3 lg3>
+                     <v-flex sm12 xs12 xl9 lg9>
+                        <v-card-text>
+                            <span class="fontIran">در مورد صدر کریپتو</span><br>
+                            <span class="fontsIran">صدرکریپتو یک سایت تجاری ارزهای دیجیتال برای اشخاص متفاوت است</span><br>
+                            <span class="fontIrans" color="grey darken-1">درصدرکریپتو، افراد کشورهای مختلف می توانند ارز محلی خود را به یک ارز دیگر تغییر دهند. این سایت به کاربران اجازه می دهد تا آگهی های تبلیغاتی خود را که در آنها نرخ ارز و روش های پرداخت برای خرید یا فروش ارز دیجیتال ها را تعیین می کنند، بفرستد. شما به این آگهی ها پاسخ می دهید و موافقت می کنید که شخص را به خرید ارزدیجیتال با پول نقد بفروشید یا به طور مستقیم با بانکداری آنلاین تجارت کنید. ارزهای دیجیتال ها در کیف پول اینترنتی صدر کریپتو قرار می گیرند و از آنجا می توانید بیت کوین خود را مستقیما پرداخت کنید.</span>
+                        </v-card-text>
+                    </v-flex>
+                    <v-flex sm7 xs7 md7 xl3 lg3 order-sm5>
                         <v-card class="text-xs-center">
                             <v-card-text>
                                 <router-link to="" class="textCard">چرا متفاوت هستیم؟</router-link><br>
@@ -13,13 +20,6 @@
                                 <router-link to="" class="textCard">همکاری با ما</router-link>
                             </v-card-text>
                         </v-card>
-                    </v-flex>
-                    <v-flex sm12 xs12 xl9 lg9>
-                        <v-card-text>
-                            <span class="fontIran">در مورد صدر کریپتو</span><br>
-                            <span class="fontsIran">صدرکریپتو یک سایت تجاری ارزهای دیجیتال برای اشخاص متفاوت است</span><br>
-                            <span class="fontIrans" color="grey darken-1">درصدرکریپتو، افراد کشورهای مختلف می توانند ارز محلی خود را به یک ارز دیگر تغییر دهند. این سایت به کاربران اجازه می دهد تا آگهی های تبلیغاتی خود را که در آنها نرخ ارز و روش های پرداخت برای خرید یا فروش ارز دیجیتال ها را تعیین می کنند، بفرستد. شما به این آگهی ها پاسخ می دهید و موافقت می کنید که شخص را به خرید ارزدیجیتال با پول نقد بفروشید یا به طور مستقیم با بانکداری آنلاین تجارت کنید. ارزهای دیجیتال ها در کیف پول اینترنتی صدر کریپتو قرار می گیرند و از آنجا می توانید بیت کوین خود را مستقیما پرداخت کنید.</span>
-                        </v-card-text>
                     </v-flex>
                 </v-layout>
                 <v-flex class="text-xs-center">
@@ -52,15 +52,18 @@
                     <span class="fontsIran">چگونگی دسترسی به تیم صدرکریپتو</span><br>
                     <span class="fontIrans">آدرس و دفتر ما در آدرس:</span>
                 </v-card-text>
-                    <v-card>
+                <v-flex xs6>
+                    <v-card color="blue-grey darken-3">
                         <v-card-text>
                             <span class="fontIrans">دپارتمان صدرکریپتو</span><br>
                             <span class="fontIrans">ساختمان صدر خیابان گاندی تهران ایران</span><br>
                             <span class="fontIrans">شماره تماس:09394636468</span>
                         </v-card-text>
                     </v-card>
+                </v-flex>
+                
                 <v-card-text class="pt-0">
-                    <span class="fontIrans"><v-icon color="grey lighten-1" size="18px">reply</v-icon>ارتباط با ما<router-link class="textCard" to="SupportCreateNewTicket">از طریق فرم درخواست پشتیبانی</router-link>
+                    <span class="fontIrans"><v-icon color="grey lighten-1" size="18px">reply</v-icon>ارتباط با ما<router-link class="textCard" to="SupportDrop">از طریق فرم درخواست پشتیبانی</router-link>
                     </span>
                 </v-card-text>
                 <v-card-text>
