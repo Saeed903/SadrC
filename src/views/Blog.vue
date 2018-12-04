cd5b55fcc9857034019f6ab19b08a3aaba3c26ac
saeed903 <saeed.bagheri903@gmail.com>
2018-12-04 13:28:13 +0330

    add image to project

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,5 +1,21 @@
 <template>
-    <div>
-        <p>این صفحه وبلاگ است</p>
-    </div>
+    <v-container fluid grid-list-xl>
+        <v-layout  justify-space-around>
+            <v-flex lg10 xl8>
+                <div class="divTopicImage">
+                    <img class="topicImage" src="./../../public/img/SadrCrypto/topic.jpg"/>
+                </div>
+            
+
+            </v-flex>
+        </v-layout>
+    </v-container>
 </template>
+<style>
+.topicImage{
+    width:100%;
+}
+.divTopicImage{
+    max-width:100%;
+}
+</style>
