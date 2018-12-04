200a1ed517c8c3fc8e6a25556988ef7c0d62f8ad
saeed903 <saeed.bagheri903@gmail.com>
2018-12-04 16:35:25 +0330

    change a bit

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -4,7 +4,16 @@
             <img class="topicImage" src="./../../public/img/SadrCrypto/topic.jpg"/>
             <v-card-text class="fontAweasome text-xs-center">داستانی از دنیای بیت کوین و ارزهای دیجیتال</v-card-text>
         </div>
-       
+        <v-card-text>
+            <router-link class="textCard" to="">ابزار بیشتر برای معاملات خاموش</router-link><br>
+            <span class="fontsIran">ما چندین ابزار را ایجاد کرده ایم که در موارد خاص می تواند معاملات با بیت کوین را ارزان تر کند. یکی از ویژگی های این نرم افزار تنها گزینه ی خاموش برای ابزار تجاری ما است. دیگر تگ recv در کد QR برای سپرده یا پرداخت صورتحساب است. توجه داشته باشید که این پست وبلاگ در راستای توسعه دهندگان، بازرگانان و / یا ارائه دهندگان خدمات است.</span><br>
+            <router-link class="textCard" to="">ادامه مطلب</router-link><br>
+            <div class="text-xs-left">
+                <span class="fontIrans">Jeremias</span><br>
+                <span class="fontIrans">September 28th, 2017</span><br>
+            </div>
+            
+        </v-card-text>
     </div>
 </template>
 <style>
