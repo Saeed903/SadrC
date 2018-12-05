4541270c228a293424b48c8558fd0175f0df32b7
saeed903 <saeed.bagheri903@gmail.com>
2018-12-05 10:48:11 +0330

    change the css in this page

--- src/App.vue
+++ src/App.vue
@@ -326,15 +326,26 @@
 }
 .fontIran{
     font-family:'Iranian Sans';
+    font-size:19px;
+    color:white;
 }
 .fontsIran{
+    font-family:'Iranian Sans';
+    font-size:15px;
+    color:#E0E0E0;
+}
+.fontIrans{
+    font-family:'Iranian Sans';
+    font-size:12px;
+    color:#BDBDBD;
+}
+.textCard{
     font-family:'iranian sans';
-    color:rgb(0, 153, 255);
+    color:#00E5FF;
     text-decoration:none;
 }
-.fontsIran:hover{
-    color:rgb(0, 140, 255);
+.textCard:hover{
+    color:#18FFFF;
 }
-
 </style>
 
