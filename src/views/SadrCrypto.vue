a2ae767f12bbc2425cd7f8a34eac42d1c4f25973
heydar69 <heydar.bagheri69@gmail.com>
2018-12-13 13:37:38 +0330

    clear the style and edit some a bit in this page

--- src/views/SadrCrypto.vue
+++ src/views/SadrCrypto.vue
@@ -1,65 +1,69 @@
 <template>
-<div>
-<v-flex>
-    <p class="iran text-xs-center " >صدرکریپتو ساده ترین و مطمعن ترین راه برای خرید و فروش و مدیریت ارز دیجیتال</p>
-    <p class="fontIran text-xs-center orange--text">فوری.امن.خصوصی</p>
-    <p class="text-xs-center ">
-        <span class="iran orange--text">تجارت بیت کوین</span>
-        <router-link to="/BitcoinCity" class="iran router">در 16370 شهر</router-link>
-        <span class="iran orange--text">و</span>
-        <router-link to="/BitcoinCountry" class="iran router">248 کشور</router-link>
-        <span class="iran orange--text">شامل</span>
-        <router-link to="/RepublicIran" class="iran router">جمهوری اسلامی ایران</router-link>
-    </p>
-    <div class="text-xs-center">
-        <v-btn class="iran" color="primary" to="Signup" round><v-icon size="20px">done_all</v-icon>ثبت نام رایگان</v-btn>
-    </div>
-</v-flex>
+    <div>
+        <v-flex>
+            <p class="fontsIran text-xs-center" >صدرکریپتو ساده ترین و مطمعن ترین راه برای خرید و فروش و مدیریت ارز دیجیتال</p>
+            <p class="fontsIran text-xs-center orange--text">فوری.امن.خصوصی</p>
+            <p class="text-xs-center ">
+                <span class="fontsIran orange--text">تجارت بیت کوین</span>
+                <router-link to="/BitcoinCity" class="textCard">در 16370 شهر</router-link>
+                <span class="fontsIran orange--text">و</span>
+                <router-link to="/BitcoinCountry" class="textCard">248 کشور</router-link>
+                <span class="fontsIran orange--text">شامل</span>
+                <router-link to="/RepublicIran" class="textCard">جمهوری اسلامی ایران</router-link>
+            </p>
+            <div class="text-xs-center">
+                <v-btn class="fontsIran elevation-24" color="cyan darken-4" to="Signup" round><v-icon size="20px">done_all</v-icon>ثبت نام رایگان</v-btn>
+            </div>
+        </v-flex>
 
-<v-flex pt-0>
-    <FormSell/>
-</v-flex>
+        <v-flex class="mt-0 mb-5">
+                <FormSell></FormSell>
+        </v-flex>
 
-<v-flex pt-0>
-    <v-card height="100%" color="grey darken-1">
-        <v-card-text class="iran text-xs-center ">خرید بیت کوین آنلاین در جمهوری اسلامی ایران</v-card-text>
-    </v-card>
-        <Advertise></Advertise>
-    </v-flex>
-
-    <v-flex>
-        <v-card color="grey darken-1">
-            <v-card-text class="iran text-xs-center ">خرید بیت کوین آنلاین در جمهوری اسلامی ایران</v-card-text>
-        </v-card>
-        <Advertise></Advertise>
-    </v-flex>
-
-    <v-flex>
-            <v-card class="text-xs-center">
-                <v-card-text class="iran text-xs-center grey darken-1">اخبار صدر سیستم</v-card-text>
-                <v-divider></v-divider>
-
-                <v-card-text class="contant text-xs-center"><router-link class="router" to="">ابزار بیشتر برای معاملات خاموش</router-link></v-card-text>
-                <v-divider></v-divider>
-
-                <v-card-text class="contant text-xs-center"><router-link class="router" to="">تغییرات صفحه ورود به سیستم</router-link></v-card-text>
-                <v-divider></v-divider>
-
-                <v-card-text class="contant text-xs-center"><router-link class="router" to="">جبران خسارت برای کاربران دارای تعادل نقدی</router-link></v-card-text>
-                <v-divider></v-divider>
-
-                <v-card-text class="contant text-xs-center"><router-link class="router" to="">سیستم جدید هزینه تراکنش برای بیت کوین</router-link></v-card-text>
-                <v-divider></v-divider>
-                
-                <v-card-text class="contant text-xs-left">اطلاعات بیشتر در<router-link class="router" to=""> وبلاگ صدر سیستم </router-link></v-card-text>
+        <v-flex pt-0>
+            <v-card height="100%" color="grey darken-1">
+                <v-card-text class="fontsIran text-xs-center ">خرید بیت کوین آنلاین در جمهوری اسلامی ایران</v-card-text>
             </v-card>
-    </v-flex>
+            <v-card class="elevation-24">
+                <Advertise></Advertise>
+            </v-card>
+            </v-flex>
 
-    {{publishAdvertise}}
-    <v-flex>
-        <Footer></Footer>
-    </v-flex>
-</div>
+            <v-flex>
+                <v-card color="grey darken-1">
+                    <v-card-text class="fontsIran text-xs-center ">فروش بیت کوین آنلاین در جمهوری اسلامی ایران</v-card-text>
+                </v-card>
+                <v-card class="elevation-24">
+                    <Advertise></Advertise>
+                </v-card>
+            </v-flex>
+
+            <v-flex>
+                    <v-card class="text-xs-center elevation-24">
+                        <v-card-text class="fontIran text-xs-center grey darken-1">اخبار صدر سیستم</v-card-text>
+                        <v-divider></v-divider>
+
+                        <v-card-text class="contant text-xs-right"><router-link class="textCard" to="">ابزار بیشتر برای معاملات خاموش</router-link></v-card-text>
+                        <v-divider></v-divider>
+
+                        <v-card-text class="contant text-xs-right"><router-link class="textCard" to="">تغییرات صفحه ورود به سیستم</router-link></v-card-text>
+                        <v-divider></v-divider>
+
+                        <v-card-text class="contant text-xs-right"><router-link class="textCard" to="">جبران خسارت برای کاربران دارای تعادل نقدی</router-link></v-card-text>
+                        <v-divider></v-divider>
+
+                        <v-card-text class="contant text-xs-right"><router-link class="textCard" to="">سیستم جدید هزینه تراکنش برای بیت کوین</router-link></v-card-text>
+                        <v-divider></v-divider>
+                        
+                        <v-card-text class="fontIrans text-xs-left">اطلاعات بیشتر در<router-link class="textCard" to=""> وبلاگ صدر سیستم </router-link></v-card-text>
+                    </v-card>
+            </v-flex>
+
+            {{publishAdvertise}}
+            <v-flex>
+                <Footer></Footer>
+        </v-flex>
+    </div>
 </template>
 <script>
 import FormSell from './../components/FormSell.vue'
@@ -103,24 +107,3 @@ export default{
     }
   };
 </script>
-<style scoped>
-.iran{
-    font-family: 'Iranian Sans';
-}
-.irans{
-    font-family: 'Iranian Sans';
-    font-size:16px;
-}
-.fontIran{
-    font-family:iranian sans;
-    font-size:17px;
-}
-.router{
-    font-family:'iranian sans';
-    color:rgb(0, 153, 255);
-    text-decoration:none;
-}
-.router:hover{
-    color:rgb(0, 140, 255);
-}
-</style>
