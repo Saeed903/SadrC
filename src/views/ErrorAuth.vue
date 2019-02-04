7f0cb6b9fe502ab38e31e1a2b6af19966cd21e9c
farzad8326 <farzad6238afsari@gmail.com>
2019-02-04 07:03:32 -0800

    changes in styles of this page

--- src/views/ErrorAuth.vue
+++ src/views/ErrorAuth.vue
@@ -1,7 +1,9 @@
 <template>
     <div>
-        <p class="fontIran"><v-icon color="red" size="20px">report_problem</v-icon>شما به این صفحه دسترسی ندارید</p>
-        <v-btn class="fontIran elevation-20" @click="goLoginPage()" color="cyan accent-2" outline>برو به صفحه ورود کاربر</v-btn>
+        <div class="text-xs-center">
+            <p class="fontIran"><v-icon color="red" size="20px">report_problem</v-icon>شما به این صفحه دسترسی ندارید</p>
+            <v-btn class="fontIran elevation-20" @click="goLoginPage()" color="cyan accent-2" outline>برو به صفحه ورود کاربر</v-btn>
+        </div>
     </div>
 </template>
 <script>
