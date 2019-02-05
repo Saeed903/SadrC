feae8d68981914ffbb2f467737bf345d3efdd6f9
farzad8326 <farzad6238afsari@gmail.com>
2019-02-05 04:35:56 -0800

    nothing importent.just for testing some thing

--- src/components/FarzadTest.vue
+++ src/components/FarzadTest.vue
@@ -1,27 +1,34 @@
 <template>
   <div class="text-xs-center">
-    <v-toolbar-side-icon
-      dark
-      color="primary"
-      @click="expand = !expand"
-      slot="activator"
-    ></v-toolbar-side-icon>
-
-    <v-expand-transition>
-      <div v-if="expand">
-        <v-btn flat>راهنما</v-btn>
-        <v-btn flat>ثبت آگهی</v-btn><br/>
-        <v-btn flat>انجمن ها</v-btn><br/>
-        <v-btn flat>کیف پول</v-btn><br/>
-        <v-btn flat>داشبورد</v-btn>
-      </div>
-    </v-expand-transition>
+    <v-btn
+    :loading="loading3"
+    :disabled="loading3"
+    color="blue-grey"
+    class="white--text"
+    @click="loader = 'loading3'"
+    >
+    Upload
+      <v-icon right dark>cloud_upload</v-icon>
+    </v-btn>
   </div>
 </template>
 <script>
   export default {
-    data: () => ({
-      expand: false
-    })
+    data () {
+      return {
+        loader: null,
+        loading3: false,
+      }
+    },
+    watch: {
+      loader () {
+        const l = this.loader
+        this[l] = !this[l]
+
+        setTimeout(() => (this[l] = false), 3000)
+
+        this.loader = null
+      }
+    }
   }
 </script>
