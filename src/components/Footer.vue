fb1788a162ee3b2de9956e988f8785cc8ea3490d
farzad8326 <farzad6238afsari@gmail.com>
2019-02-10 07:22:43 -0800

    create footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,41 +1,62 @@
 <template>
-  <div>
-    <v-footer height="auto">
-      <v-card class="flex" flat tile>
-        <v-layout>
-            <v-flex class="pr-3">
-              <v-card-text row wrap>
-                <p>farzad</p>
-                <p>farzad</p>
-                <p>farzad</p>
-                <p>farzad</p>
-                <p>farzad</p>
-              </v-card-text>
-            </v-flex>
-            <v-flex class="pr-3">
-              <v-card-text row wrap>
-                <p>farzad</p>
-                <p>farzad</p>
-                <p>farzad</p>
-                <p>farzad</p>
-                <p>farzad</p>
-              </v-card-text>
-            </v-flex>
-            <v-flex class="pr-3">
-              <v-card-text row wrap>
-                
-                
-              </v-card-text>
-              
-            </v-flex>
+  <v-footer dark height="auto">
+    <v-card class="flex">
+      <v-icon color="cyan accent-2" size="18px">reply_all</v-icon><span class="newsText text-xs-center">صدرکریپتو ساده ترین راه برای خرید و فروش و مدیریت ارز دیجیتال خود</span>
+      <v-card-title class="">
+        <v-layout row>
+          <v-flex>
+            <ul>
+              <li class="newsText">درباره ما</li>
+              <li class="newsText">درباره ما</li>
+              <li class="newsText">شغل</li>
+              <li class="newsText">هزینه ها</li>
+              <li class="newsText">اسناد امنیتی</li>
+              <li class="newsText">آمار</li>
+              <li class="newsText">شرایط استفاده</li>
+              <li class="newsText">حریم خصوصی</li>
+            </ul>
+          </v-flex>
+          <v-flex>
+            <ul>
+              <li class="newsText">پشتیبانی</li>
+              <li class="newsText">farzad</li>
+              <li class="newsText">farzad</li>
+              <li class="newsText">farzad</li>
+              <li class="newsText">farzad</li>
+            </ul>
+          </v-flex>
+          <v-flex>
+            <ul>
+              <li class="newsText">خدمات</li>
+              <li class="newsText">farzad</li>
+              <li class="newsText">farzad</li>
+              <li class="newsText">farzad</li>
+              <li class="newsText">farzad</li>
+            </ul>
+          </v-flex>
+          
+          <v-flex>
+            <ul>
+              <li class="newsText">مجازی</li>
+              <li class="newsText">farzad</li>
+              <li class="newsText">farzad</li>
+              <li class="newsText">farzad</li>
+              <li class="newsText">farzad</li>
+            </ul>
+          </v-flex>
+       
         </v-layout>
-        <v-spacer></v-spacer>
-        <v-card-actions class="justify-center">
-          <strong class="b Nazanin subheading">copy right all right reserved by sadrCrypto</strong> — 2018&copy;
-        </v-card-actions>
-      </v-card>
-    </v-footer>
-  </div>
+      </v-card-title>
+
+      <v-card-actions class="grey darken-3 justify-center">
+         &copy;2018 —2019 <span class="footerBottom">sadrCrypto </span>
+      </v-card-actions>
+      <v-divider></v-divider>
+      <v-icon color="cyan accent-2" size="18px">reply_all</v-icon><span class="newsText"> شرایط استفاده از سایت</span>
+      <v-spacer></v-spacer>
+      <v-icon color="cyan accent-2" size="18px">reply_all</v-icon><span class="newsText"> قوانین و مقررات سایت</span>
+    </v-card>
+  </v-footer>
 </template>
 <script>
   export default {
@@ -47,13 +68,17 @@
         'fab fa-linkedin',
         'fab fa-instagram'
       ]
-    }),
-  
+    })
   }
 </script>
 <style>
-.bNazanin{
+.footerBottom{
   font-family:b nazanin;
-  font-size:25px
+  font-size:18px;
+}
+.newsText{
+    font-family:iranian sans;
+    font-size:13px;
+    color:#BDBDBD;
 }
 </style>
