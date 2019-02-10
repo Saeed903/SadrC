f1a9de5956d15e0429dcff271a9ef26f6495a5cb
farzad8326 <farzad6238afsari@gmail.com>
2019-02-10 07:23:06 -0800

    just for testing

--- src/components/FarzadTest.vue
+++ src/components/FarzadTest.vue
@@ -1,45 +1,45 @@
 <template>
-<div>
-  <v-layout row>
-    <v-flex xs10 sm6 md4 lg3 xl4>
-      <v-card class="elevation-20">
-        <!--<img class="cardImg" src="./../../public/img/SadrCrypto/blockchain.jpg">-->
-        <v-card-title primary-title>
-          <div>
-            <div class="fontsIran">ادغام API ما</div>
-            <span class="fontsIran grey--text">یک راه حل کاملا استاندارد سفید برای پروژه شما، یعنی کیسه کیف پول، بازار یا ارائه دهنده خدمات. تنظیم آن را به پذیرفتن هر 100 + ارزدیجیتال  ذکر شده در صدرکریپتو و دریافت درآمد از هر معامله ساخته شده است.</span>
-          </div>
-        </v-card-title>
+  <v-footer
+    dark
+    height="auto"
+  >
+    <v-card
+      class="flex"
+      flat
+      tile
+    >
+      <v-card-title class="teal">
+        <strong class="subheading">Get connected with us on social networks!</strong>
 
-        <v-card-actions>
-          <v-btn class="fontsIran elevation-24" color="cyan accent-2" outline>اطلاعات بیشتر</v-btn>
-          <v-spacer></v-spacer>
-          <v-btn icon @click="show = !show">
-            <v-icon color="cyan accent-2">{{ show ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
-          </v-btn>
-        </v-card-actions>
+        <v-spacer></v-spacer>
 
-        <v-slide-y-transition>
-          <v-card-text class="fontsIran grey--text" v-show="show">
-            یک راه حل کاملا استاندارد سفید برای پروژه شما، یعنی کیسه کیف پول، بازار یا ارائه دهنده خدمات. تنظیم آن را به پذیرفتن هر 100 + ارزدیجیتال  ذکر شده در صدرکریپتو و دریافت درآمد از هر معامله ساخته شده است.
-          </v-card-text>
-        </v-slide-y-transition>
-      </v-card>
-    </v-flex>
-  </v-layout>
-  </div>
+        <v-btn
+          v-for="icon in icons"
+          :key="icon"
+          class="mx-3"
+          dark
+          icon
+        >
+          <v-icon size="24px">{{ icon }}</v-icon>
+        </v-btn>
+      </v-card-title>
+
+      <v-card-actions class="grey darken-3 justify-center">
+        &copy;2018 — <strong>Vuetify</strong>
+      </v-card-actions>
+    </v-card>
+  </v-footer>
 </template>
 <script>
   export default {
     data: () => ({
-      show: false
+      icons: [
+        'fab fa-facebook',
+        'fab fa-twitter',
+        'fab fa-google-plus',
+        'fab fa-linkedin',
+        'fab fa-instagram'
+      ]
     })
   }
 </script>
-<style>
-.cardImg{
-  width:450px;
-  max-width:100%;
-}
-
-</style>
