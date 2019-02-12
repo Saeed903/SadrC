0a35c74e21d2406c53676cc352d7b2c65221f0d1
farzad8326 <farzad6238afsari@gmail.com>
2019-02-12 06:28:16 -0800

    just for testing

--- src/components/FarzadTest.vue
+++ src/components/FarzadTest.vue
@@ -1,30 +1,56 @@
 <template>
-  <v-footer
-    dark
-    height="auto"
-  >
-    <v-card
-      class="flex"
-      flat
-      tile
-    >
-      <v-card-title class="teal">
+  <v-footer height="auto">
+    <v-card flat tile>
+      <v-card-title class="teal white--text">
         <strong class="subheading">Get connected with us on social networks!</strong>
-
         <v-spacer></v-spacer>
-
         <v-btn
           v-for="icon in icons"
           :key="icon"
-          class="mx-3"
-          dark
           icon
+          dark
+          class="mx-3"
         >
           <v-icon size="24px">{{ icon }}</v-icon>
         </v-btn>
       </v-card-title>
-
-      <v-card-actions class="grey darken-3 justify-center">
+      <v-card-text class="grey lighten-3">
+        <v-layout>
+          <v-flex
+            v-for="(col, i) in rows"
+            :key="i"
+            xs3
+          >
+            <span class="body-2 black--text" v-text="col.title.toUpperCase()"/>
+            <div
+              v-for="(child, i) in col.children"
+              :key="i"
+              v-text="child"
+              class="black--text"
+            />
+          </v-flex>
+          <v-flex xs3 layout column>
+            <span class="body-2">CONTACT</span>
+            <div class="black--text">
+              <v-icon size="18px" class="mr-3 black--text">fas fa-home</v-icon>
+              New York, NY 10012, US
+            </div>
+            <div class="black--text">
+              <v-icon size="18px" class="mr-3">fas fa-envelope</v-icon>
+              info@example.com
+            </div>
+            <div class="black--text">
+              <v-icon size="18px" class="mr-3">fas fa-phone</v-icon>
+              + 01 234 567 88
+            </div>
+            <div class="black--text">
+              <v-icon size="18px" class="mr-3">fas fa-print</v-icon>
+              + 01 234 567 89
+            </div>
+          </v-flex>
+        </v-layout>
+      </v-card-text>
+      <v-card-actions class="grey lighten-2 justify-center">
         &copy;2018 — <strong>Vuetify</strong>
       </v-card-actions>
     </v-card>
@@ -33,12 +59,20 @@
 <script>
   export default {
     data: () => ({
-      icons: [
-        'fab fa-facebook',
-        'fab fa-twitter',
-        'fab fa-google-plus',
-        'fab fa-linkedin',
-        'fab fa-instagram'
+      icons: ['fab fa-facebook', 'fab fa-twitter', 'fab fa-google-plus', 'fab fa-linkedin', 'fab fa-instagram'],
+      rows: [
+        {
+          title: 'Company Name',
+          children: ['Here you can use rows and columns here to organize your footer content. Lorem ipsum dolor sit amet, consectetur adipisicing elit']
+        },
+        {
+          title: 'Products',
+          children: ['MDBootstrap', 'MDWordPress', 'BrandFlow', 'Bootstrap Angular']
+        },
+        {
+          title: 'Useful Links',
+          children: ['Your account', 'Become an Affiliate', 'Shipping Rates', 'Helper']
+        }
       ]
     })
   }
