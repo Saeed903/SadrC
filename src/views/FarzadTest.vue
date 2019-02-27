943a4a47f4421587fd1707303a066b35d389ec8a
farzad8326 <farzad6238afsari@gmail.com>
2019-02-27 14:22:20 +0330

    edit and finish this pages according to the plan

---
+++ src/views/FarzadTest.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <v-hover>
+      <v-card slot-scope="{ hover }" class="mx-auto" color="grey lighten-4" max-width="600">
+        <div style="background-color:grey;">
+          <v-card-text class="white--text">farzad afsari</v-card-text>
+          <v-card-text class="white--text">farzad afsari</v-card-text>
+          <v-card-text class="white--text">farzad afsari</v-card-text>
+          <v-card-text class="white--text">farzad afsari</v-card-text>
+          <v-card-text class="white--text">farzad afsari</v-card-text>
+          <v-card-text class="white--text">farzad afsari</v-card-text>
+          <v-card-text class="white--text">farzad afsari</v-card-text>
+          <v-card-text class="white--text">farzad afsari</v-card-text>
+          <v-card-text class="white--text">farzad afsari</v-card-text>
+          <v-card-text class="white--text">farzad afsari</v-card-text>
+        </div>
+        <v-expand-transition>
+        <div
+        v-if="hover"
+        class="d-flex transition-fast-in-fast-out grey darken-2 v-card--reveal display-3 white--text"
+        style="height: 100%;"
+        >
+        farzad afsari
+        </div>
+        </v-expand-transition>
+      </v-card>
+    </v-hover>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
--- src/views/SupportCreateNewTicket.vue
+++ src/views/SupportCreateNewTicket.vue
@@ -1,35 +1,47 @@
 <template>
-   <div>
-        <v-card-text class="fontIran">بلیط های پشتیبانی من</v-card-text>
-        <v-flex>
-            <v-card color="blue-grey darken-3 elevation-15">
-                <v-card-text class="fontIrans text-xs-right"><v-icon color="orange lighten-3" size="17px">done_all</v-icon>کاربران عزیز، با توجه به مقدار غیر معمول بلیط، زمان پاسخ فعلی ما 2 روز است. با مهربانی از شما می خواهیم که بلیط های چندگانه را در رابطه با همین مسئله باز نکنید، زیرا فقط زمان پاسخ ما را طولانی تر می کند.</v-card-text>
-            </v-card>
-            <v-card-text class="fontIrans text-xs-right"><v-icon color="red" size="17px">warning</v-icon>لطفا یک لحظه از وقت خود را برای توضیح مسئله خود بیاموزید. اگر هیچ یک از گزینه های زیر با موضوع شما مطابقت نداشته باشد، لطفا "دیگر" را انتخاب کنید و در قسمت نوشتن توصیف کنید</v-card-text>
-        </v-flex>
-        <v-card-text class="fontIran">لطفا بهترین گزینه را انتخاب کنید.</v-card-text><br>
+    <div>
+        <v-card-text class="fontIran text-xs-center pt-5">بلیط های پشتیبانی من</v-card-text>
+        <v-card-text class="describeText"><v-icon color="red" size="25px">alarm_add</v-icon>لطفا یک لحظه از وقت خود را برای توضیح مسئله خود بیاموزید. اگر هیچ یک از گزینه های زیر با موضوع شما مطابقت نداشته باشد، لطفا "دیگر" را انتخاب کنید و در قسمت نوشتن توصیف کنید</v-card-text>
+
+        <v-card-text class="fontIran pb-0 pt-5">لطفا بهترین گزینه را انتخاب کنید.</v-card-text><br>
+        <v-divider color="grey"></v-divider>
         <v-layout row wrap>
-            <v-flex sm3 md3 lg3 xl3>
-                    <v-card-text class="pt-0"><router-link class="textCard" to=""><v-icon color="grey" size="20px">message</v-icon>گزارش تقلب</router-link><br></v-card-text>
-                    <v-card-text class="pt-0"><router-link class="textCard" to=""><v-icon color="grey" size="20px">message</v-icon>با یک تجارت مشکوک کمک کنید</router-link><br></v-card-text>
-                    <v-card-text class="pt-0"><router-link class="textCard" to=""><v-icon color="grey" size="20px">message</v-icon>مشکلات با معامله بیت کوین</router-link><br></v-card-text>
-                    <v-card-text class="pt-0"><router-link class="textCard" to=""><v-icon color="grey" size="20px">message</v-icon>حساب من هک شده است</router-link><br></v-card-text>
-                    <v-card-text class="pt-0"><router-link class="textCard" to=""><v-icon color="grey" size="20px">message</v-icon>کاربر جدید هستم</router-link><br></v-card-text>
-                    <v-card-text class="pt-0"><router-link class="textCard" to=""><v-icon color="grey" size="20px">message</v-icon>کاربر جدید هستم</router-link><br></v-card-text>
-                    <v-card-text class="pt-0"><router-link class="textCard" to=""><v-icon color="grey" size="20px">message</v-icon>حساب من در تعطیلات اجباری است</router-link><br></v-card-text>
+            <v-flex>
+                <v-card-text class="pt-0"><v-icon class="pl-1" color="grey" size="20px">email</v-icon><router-link class="textCard" to="">گزارش تقلب</router-link><br></v-card-text>
+                <v-card-text class="pt-0"><v-icon class="pl-1" color="grey" size="20px">email</v-icon><router-link class="textCard" to="">با یک تجارت مشکوک کمک کنید</router-link><br></v-card-text>
+                <v-card-text class="pt-0"><v-icon class="pl-1" color="grey" size="20px">email</v-icon><router-link class="textCard" to="">مشکلات با معامله بیت کوین</router-link><br></v-card-text>
+                <v-card-text class="pt-0"><v-icon class="pl-1" color="grey" size="20px">email</v-icon><router-link class="textCard" to="">حساب من هک شده است</router-link><br></v-card-text>
+                <v-card-text class="pt-0"><v-icon class="pl-1" color="grey" size="20px">email</v-icon><router-link class="textCard" to="">کاربر جدید هستم</router-link><br></v-card-text>
+                <v-card-text class="pt-0"><v-icon class="pl-1" color="grey" size="20px">email</v-icon><router-link class="textCard" to="">کاربر جدید هستم</router-link><br></v-card-text>
+                <v-card-text class="pt-0"><v-icon class="pl-1" color="grey" size="20px">email</v-icon><router-link class="textCard" to="">حساب من در تعطیلات اجباری است</router-link><br></v-card-text>
             </v-flex>
-            <v-flex sm3 md3 lg3 xl3>
-                    <v-card-text class="pt-0"><router-link class="textCard" to=""><v-icon color="grey" size="20px">message</v-icon>با تبلیغات من مشکل دارم</router-link><br></v-card-text>
-                    <v-card-text class="pt-0"><router-link class="textCard" to=""><v-icon color="grey" size="20px">message</v-icon>من بازخورد خشونت آمیز دریافت کرده ام</router-link><br></v-card-text>
-                    <v-card-text class="pt-0"><router-link class="textCard" to=""><v-icon color="grey" size="20px">message</v-icon>گزارش یک اشکال</router-link><br></v-card-text>
-                    <v-card-text class="pt-0"><router-link class="textCard" to=""><v-icon color="grey" size="20px">message</v-icon>من نمیتوانم وارد حساب کاربری شوم</router-link><br></v-card-text>
-                    <v-card-text class="pt-0"><router-link class="textCard" to=""><v-icon color="grey" size="20px">message</v-icon>ایمیل تأیید حساب من دریافت نمی کنم</router-link><br></v-card-text>
-                    <v-card-text class="pt-0"><router-link class="textCard" to=""><v-icon color="grey" size="20px">message</v-icon>من نمیتوانم شماره تلفنم را تأیید کنم</router-link><br></v-card-text>
-                    <v-card-text class="pt-0"><router-link class="textCard" to=""><v-icon color="grey" size="20px">message</v-icon>دیگر</router-link><br></v-card-text>
+            <v-flex>
+                <v-card-text class="pt-0"><v-icon class="pl-1" color="grey" size="20px">email</v-icon><router-link class="textCard" to="">با تبلیغات من مشکل دارم</router-link><br></v-card-text>
+                <v-card-text class="pt-0"><v-icon class="pl-1" color="grey" size="20px">email</v-icon><router-link class="textCard" to="">من بازخورد خشونت آمیز دریافت کرده ام</router-link><br></v-card-text>
+                <v-card-text class="pt-0"><v-icon class="pl-1" color="grey" size="20px">email</v-icon><router-link class="textCard" to="">گزارش یک اشکال</router-link><br></v-card-text>
+                <v-card-text class="pt-0"><v-icon class="pl-1" color="grey" size="20px">email</v-icon><router-link class="textCard" to="">من نمیتوانم وارد حساب کاربری شوم</router-link><br></v-card-text>
+                <v-card-text class="pt-0"><v-icon class="pl-1" color="grey" size="20px">email</v-icon><router-link class="textCard" to="">ایمیل تأیید حساب من دریافت نمی کنم</router-link><br></v-card-text>
+                <v-card-text class="pt-0"><v-icon class="pl-1" color="grey" size="20px">email</v-icon><router-link class="textCard" to="">من نمیتوانم شماره تلفنم را تأیید کنم</router-link><br></v-card-text>
+                <v-card-text class="pt-0"><v-icon class="pl-1" color="grey" size="20px">email</v-icon><router-link class="textCard" to="">دیگر</router-link><br></v-card-text>
             </v-flex>
         </v-layout>
-   </div>
+       
+</div>
 </template>
-<style scoped>
+<script>
+export default {
 
+}
+</script>
+<style scoped>
+.logCard{
+  border:1px solid #18FFFF;
+  box-shadow:0px 0px 15px 1px #18FFFF;
+}
+.describeText{
+    font-family:iranian sans;
+    font-size:15px;
+    color:#E0E0E0;
+    text-align:justify;
+}
 </style>
