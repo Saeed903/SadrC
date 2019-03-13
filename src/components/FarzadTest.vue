7b58276156ceed0143d1a6e12201bd28c04de6f0
farzad8326 <farzad6238afsari@gmail.com>
2019-03-13 12:24:48 +0330

    farzad test

--- src/components/FarzadTest.vue
+++ src/components/FarzadTest.vue
@@ -1,63 +1,39 @@
 <template>
-  <div>
-    <v-hover>
-      <v-card slot-scope="{ hover }" class="mx-auto" color="grey lighten-4" max-width="600">
-        <div style="background-color:grey;">
-          <v-card-text class="white--text">farzad afsari</v-card-text>
-          <v-card-text class="white--text">farzad afsari</v-card-text>
-          <v-card-text class="white--text">farzad afsari</v-card-text>
-          <v-card-text class="white--text">farzad afsari</v-card-text>
-          <v-card-text class="white--text">farzad afsari</v-card-text>
-          <v-card-text class="white--text">farzad afsari</v-card-text>
-          <v-card-text class="white--text">farzad afsari</v-card-text>
-          <v-card-text class="white--text">farzad afsari</v-card-text>
-          <v-card-text class="white--text">farzad afsari</v-card-text>
-          <v-card-text class="white--text">farzad afsari</v-card-text>
-        </div>
-        <v-expand-transition>
-        <div
-        v-if="hover"
-        class="d-flex transition-fast-in-fast-out grey darken-2 v-card--reveal display-3 white--text"
-        style="height: 100%;"
-        >
-        farzad afsari
-        </div>
-        </v-expand-transition>
-      </v-card>
-    </v-hover>
+<div>
+  <v-app id="inspire">
+    <v-subheader>
+      <v-btn @click="addBug()">Add Bugs</v-btn>
+    </v-subheader>
+    <v-radio-group v-model="selected" :mandatory="false">
+      <v-radio v-for="(l, i) in data" :key="i" :label="l.name" :value="l.value" @change="getValue(l.value)"></v-radio>
+      <v-radio v-if="bugs" v-for="(l, i) in bugs" :key="i" :label="l.name" :value="l.value" @change="getValue(l.value)"></v-radio>
+    </v-radio-group>
+  </v-app>
+</div>
 
-    <v-layout>
-        <v-flex>
-            <v-card>
-                <p>farzad</p>
-            </v-card>
-        </v-flex>
-        <v-flex>
-            <v-card>
-                <p>farzad</p>
-            </v-card>
-        </v-flex>
-        <v-flex>
-            <v-card>
-                <p>farzad</p>
-            </v-card>
-        </v-flex>
-    </v-layout>
-
-  </div>
 </template>
 <script>
 export default {
-
+     data:()=> ({
+        selected: 'radio-1',
+        clickVal: null,
+        data: [
+         
+        ],
+        bugs: [],
+    }),
+     methods: {
+    addBug() {
+      this.bugs = [
+        { name: 'Lady Bettle', value: 'bug 1' },
+        { name: 'Hercules beetle', value: 'bug 2' },
+      ];
+    },
+    getValue(v) {
+      this.clickVal = v;
+      // to workaround, uncomment lime below
+      // this.selected = v;
+    },
+  },
 }
 </script>
-<style scoped>
-.v-card--reveal {
-  align-items: center;
-  bottom: 0;
-  justify-content: center;
-  opacity: .5;
-  position: absolute;
-  width: 100%;
-}
-</style>
