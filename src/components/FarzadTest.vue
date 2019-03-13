5c0d689bbb7566404d0eb01724eccafce5f1a647
heydar69 <heydar.bagheri69@gmail.com>
2019-03-13 12:58:40 +0330

    add radio button

--- src/components/FarzadTest.vue
+++ src/components/FarzadTest.vue
@@ -1,40 +1,25 @@
-<template>
-    <div>
-            <v-radio-group>
-                <v-radio @click="addBug()">Add Bugs</v-radio>
-            </v-radio-group>
-
-            <v-card v-model="selected" v-if="bugs" v-for="(l, i) in bugs" :key="i" :label="l.name" :value="l.value" @change="getValue(l.value)">
-                <v-text-field label="farzad"></v-text-field>
-                <v-text-field label="farzad"></v-text-field>
-                <v-text-field label="farzad"></v-text-field>
-                <v-btn>farzad</v-btn>
-            </v-card>
-
-    </div>
+<template lang="pug">
+  div
+    v-radio-group(v-model="selectedTimeWork")
+      v-radio(value="everyDay" label="ساعات کاری هفته")
+      v-radio(value="SelectTime" label="انتخاب ساعتهای کاری آنلاین")
+    v-card(v-if="showSelectedHours")  
+      v-btn farzad
 </template>
 <script>
 export default {
-     data:()=> ({
-        selected: 'radio-1',
-        clickVal: null,
-        data: [
-         
-        ],
-        bugs: [],
-    }),
-     methods: {
-    addBug() {
-      this.bugs = [
-        { name: 'Lady Bettle', value: 'bug 1' },
-        { name: 'Hercules beetle', value: 'bug 2' },
-      ];
-    },
-    getValue(v) {
-      this.clickVal = v;
-      // to workaround, uncomment lime below
-      // this.selected = v;
-    },
+  name:'FarzadTest',
+  data:()=> ({
+    selectedTimeWork: "everyDay",
+    data: [
+      
+    ],
+    bugs: [],
+  }),
+  computed:{
+    showSelectedHours(){
+      return this.selectedTimeWork== "SelectTime"?true:false;
+    }
   },
 }
 </script>
